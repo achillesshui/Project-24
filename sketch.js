fd2f6c6228d33101c4107df8105a2418ad07852f
achillesshui <56407377+achillesshui@users.noreply.github.com>
2020-11-19 20:42:46 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -51,7 +51,7 @@ function draw() {
 
 function keyPressed() {
 	if(keyCode == UP_ARROW) {
-		Matter.Body.applyForce(paper.body, paper.position, {x:85, y:-85})
+		Matter.Body.applyForce(paper.body, paper.position, {x:640, y:645})
 	}
 }
 
